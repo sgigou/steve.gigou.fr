c3f806c035a45b1d46e629b656dcf048dc3c7c4d
Steve Gigou <steve@account.gigou.fr>
2020-02-24 13:08:04 +0100

    Common RPLife footers

---
+++ footer-rplife.php
@@ -0,0 +1,28 @@
+<footer class="grid-container">
+  <div class="grid-100 grid-parent">
+    <div class="grid-25 list margin-bottom-16">
+      <h4>Informations</h4>
+      <p><a href="rplife-changelog.php">Changelog</a></p>
+      <p><a href="rplife-privacy.php">Privacy policy</a></p>
+    </div>
+    <div class="grid-25 list margin-bottom-16">
+      <h4>Follow RPLife</h4>
+      <p><a href="https://apps.apple.com/us/app/rplife/id1468074490" class="appstore-rplife">App Store</a></p>
+      <p><a href="https://www.facebook.com/RPLifeApp">Facebook</a></p>
+    </div>
+    <div class="grid-25 list margin-bottom-16">
+      <h4>My work</h4>
+      <p><a href="https://sgigou.github.io">Blog</a></p>
+      <p><a href="https://github.com/sgigou">GitHub</a></p>
+      <p><a href="https://apps.apple.com/us/developer/steve-gigou/id849144018">App Store</a></p>
+    </div>
+    <div class="grid-25 list">
+      <h4>Follow me</h4>
+      <p><a href="https://twitter.com/stevegigou">Twitter</a></p>
+      <p><a href="https://www.linkedin.com/in/stevegigou">LinkedIn</a></p>
+    </div>
+  </div>
+  <div class="grid-100 margin-top-16">
+    © Steve Gigou — 2019
+  </div>
+</footer>
--- rplife.php
+++ rplife.php
@@ -148,34 +148,7 @@
     </div>
   </div>
     
-  <footer class="grid-container white">
-    <div class="grid-100 grid-parent">
-      <div class="grid-25 list margin-bottom-16">
-        <h4 class="white">Informations</h4>
-        <p><a href="rplife-changelog.php">Changelog</a></p>
-        <p><a href="rplife-privacy.php">Privacy policy</a></p>
-      </div>
-      <div class="grid-25 list margin-bottom-16">
-        <h4 class="white">Follow RPLife</h4>
-        <p><a href="https://apps.apple.com/us/app/rplife/id1468074490" class="appstore-rplife">App Store</a></p>
-        <p><a href="https://www.facebook.com/RPLifeApp">Facebook</a></p>
-      </div>
-      <div class="grid-25 list margin-bottom-16">
-        <h4 class="white">My work</h4>
-        <p><a href="https://sgigou.github.io">Blog</a></p>
-        <p><a href="https://github.com/sgigou">GitHub</a></p>
-        <p><a href="https://apps.apple.com/us/developer/steve-gigou/id849144018">App Store</a></p>
-      </div>
-      <div class="grid-25 list">
-        <h4 class="white">Follow me</h4>
-        <p><a href="https://twitter.com/stevegigou">Twitter</a></p>
-        <p><a href="https://www.linkedin.com/in/stevegigou">LinkedIn</a></p>
-      </div>
-    </div>
-    <div class="grid-100 margin-top-16">
-      © Steve Gigou — 2019
-    </div>
-  </footer>
+  <?php include('footer-rplife.php') ?>
   
   <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
   <script>
