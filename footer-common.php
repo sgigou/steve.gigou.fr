9f8399d45ae754e275e25bb976f38ca74c23bc68
Steve Gigou <steve@account.gigou.fr>
2020-02-24 12:54:17 +0100

    Common footer

---
+++ footer-common.php
@@ -0,0 +1,16 @@
+<footer class="grid-container">
+  <div class="grid-50 margin-bottom-16">
+    © Steve Gigou — 2019
+  </div>
+  <div class="grid-25 list margin-bottom-16">
+    <h4>My work</h4>
+    <p><a href="https://sgigou.github.io">Blog</a></p>
+    <p><a href="https://github.com/sgigou">GitHub</a></p>
+    <p><a href="https://apps.apple.com/us/developer/steve-gigou/id849144018">App Store</a></p>
+  </div>
+  <div class="grid-25 list">
+    <h4>Follow me</h4>
+    <p><a href="https://twitter.com/stevegigou">Twitter</a></p>
+    <p><a href="https://www.linkedin.com/in/stevegigou">LinkedIn</a></p>
+  </div>
+</footer>
--- index.php
+++ index.php
@@ -115,22 +115,7 @@
       I’m also working on a <a href="https://en.wikipedia.org/wiki/Permaculture">permaculture farm</a>.</p>
   </div>
     
-  <footer class="grid-container">
-    <div class="grid-50 margin-bottom-16">
-      © Steve Gigou — 2019
-    </div>
-    <div class="grid-25 list margin-bottom-16">
-      <h4>My work</h4>
-      <p><a href="https://sgigou.github.io">Blog</a></p>
-      <p><a href="https://github.com/sgigou">GitHub</a></p>
-      <p><a href="https://apps.apple.com/us/developer/steve-gigou/id849144018">App Store</a></p>
-    </div>
-    <div class="grid-25 list">
-      <h4>Follow me</h4>
-      <p><a href="https://twitter.com/stevegigou">Twitter</a></p>
-      <p><a href="https://www.linkedin.com/in/stevegigou">LinkedIn</a></p>
-    </div>
-  </footer>
+  <?php include('footer-common.php') ?>
   
   <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
   <script>
