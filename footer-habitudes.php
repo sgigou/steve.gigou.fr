0d5ad1392627497f0c9af7fc89ad048baaea15ba
Steve Gigou <steve@account.gigou.fr>
2020-04-17 15:06:52 +0200

    Mark links to update

--- footer-habitudes.php
+++ footer-habitudes.php
@@ -7,8 +7,9 @@
     </div>
     <div class="grid-25 list margin-bottom-16">
       <h4>Follow Habitudes</h4>
-      <p><a href="https://apps.apple.com/us/app/rplife/id1468074490" class="appstore-rplife">App Store</a></p>
-      <p><a href="https://www.facebook.com/RPLifeApp">Facebook</a></p>
+      <!-- TODO: Link -->
+      <p><a href="https://apps.apple.com/us/app/rplife/id1468074490" class="appstore-habitudes">App Store</a></p>
+      <p><a href="https://www.facebook.com/HabitudesApp">Facebook</a></p>
     </div>
     <div class="grid-25 list margin-bottom-16">
       <h4>My work</h4>
