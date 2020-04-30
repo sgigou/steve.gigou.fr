fb231f51843a54fa820d942d6c7d9f0306f1ca75
Steve Gigou <steve@account.gigou.fr>
2020-04-30 17:37:49 +0200

    Better file organization

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html>
-    
+
 <head>
   <meta charset="utf-8">
   <?php include('head.php') ?>
@@ -8,159 +8,164 @@
 </head>
 
 <body class="light">
-  
+
   <?php
-    include('header.php')
+  include('header.php')
   ?>
-  
-  <div class="grid-container margin-top-64 margin-bottom-64">
-    <h1>Hi, I’m Steve!</h1>
-    <p class="subtitle">
-      I create affordable <a href="#rplife">iOS apps</a>,<br>
-      <a href="libs.php">libraries</a> for Swift<br>
-      and <a href="fonts.php">free fonts</a>.
-    </p>
-    <a href="https://apps.apple.com/us/developer/steve-gigou/id849144018">Follow me on the App Store</a>
-  </div>
-  
-  <!-- RPLife -->
-  <div id="rplife" class="grid-container block block-blue">
-    <div class="grid-33 hide-on-mobile margin-top-32" style="position: relative; display: block; height: 348px;">
-      <img src="img/rplife-home@2x.png" alt="RPLife capture" class="full-width" style="position: absolute; bottom: 0; left: 16px;">
-    </div>
-    <div class="grid-66">
-      <a class="undecored" href="rplife.php">
-        <div class="app_title margin-top-64">
-          <h2 class="white">
-            <img src="img/rplife-appicon-icon.png" alt="RPLife icon" width="48" height="48" style="transform: translateY(-2px);">
-            RPLife
-            <span>for iPhone</span>
-          </h2>
-        </div>
-      </a>
-      <h3 class="white">Roleplay your life</h3>
-      <p class="white">Evaluate your skills like in an RPG.<br>
-      Execute IRL tasks and watch your life improve.</p>
-      <div class="dl-buttons mobile-margin-bottom-32">
-        <a href="rplife.php" class="button-app">Watch more</a><a href="https://apps.apple.com/us/app/rplife/id1468074490" class="appstore-rplife"><img src="img/appstore.svg" alt="Download RPLife on the App Store"></a>
-      </div>
-    </div>
-    <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop">
-      <img src="img/rplife-home@2x.png" alt="RPLife capture" class="full-width">
-    </div>
-  </div>
-  
-  <!-- Habitudes -->
-  <div class="grid-container block block-yellow">
-    <div class="grid-66">
-      <a class="undecored" href="habitudes.php">
-        <div class="app_title margin-top-64">
-          <h2 class="white">
-            <img src="img/habitudes-icon@2x.png" alt="RPLife icon" width="48" height="48" style="transform: translateY(-2px);">
-            Habitudes
-            <span>for iPhone</span>
-          </h2>
-        </div>
-      </a>
-      <h3 class="white">We are what we repeatedly do</h3>
-      <p class="white">
-        Use the power of habits and routines<br>
-        to improve yourself every day.
+
+  <div class="grid-container">
+
+    <!-- Intro -->
+    <div class="margin-top-64 margin-bottom-64">
+      <h1>Hi, I’m Steve!</h1>
+      <p class="subtitle">
+        I create affordable <a href="#rplife">iOS apps</a>,<br>
+        <a href="libs.php">libraries</a> for Swift<br>
+        and <a href="fonts.php">free fonts</a>.
       </p>
-      <div class="dl-buttons mobile-margin-bottom-32">
-        <a href="habitudes.php" class="button-app">Watch more</a><a href="https://apps.apple.com/us/app/habitudes/id1500560167" class="appstore-habitudes"><img src="img/appstore.svg" alt="Download Habitudes on the App Store"></a>
+      <a href="https://apps.apple.com/us/developer/steve-gigou/id849144018">Follow me on the App Store</a>
+    </div>
+
+    <!-- RPLife -->
+    <div id="rplife" class="grid-100 block block-blue">
+      <div class="grid-33 hide-on-mobile margin-top-32" style="position: relative; display: block; height: 348px;">
+        <img src="img/rplife-home@2x.png" alt="RPLife capture" class="full-width" style="position: absolute; bottom: 0; left: 16px;">
+      </div>
+      <div class="grid-66">
+        <a class="undecored" href="rplife.php">
+          <div class="app_title margin-top-64">
+            <h2 class="white">
+              <img src="img/rplife-appicon-icon.png" alt="RPLife icon" width="48" height="48" style="transform: translateY(-2px);">
+              RPLife
+              <span>for iPhone</span>
+            </h2>
+          </div>
+        </a>
+        <h3 class="white">Roleplay your life</h3>
+        <p class="white">Evaluate your skills like in an RPG.<br>
+        Execute IRL tasks and watch your life improve.</p>
+        <div class="dl-buttons mobile-margin-bottom-32">
+          <a href="rplife.php" class="button-app">Watch more</a><a href="https://apps.apple.com/us/app/rplife/id1468074490" class="appstore-rplife"><img src="img/appstore.svg" alt="Download RPLife on the App Store"></a>
+        </div>
+      </div>
+      <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop">
+        <img src="img/rplife-home@2x.png" alt="RPLife capture" class="full-width">
       </div>
     </div>
-    <div class="grid-33 hide-on-mobile margin-top-32" style="position: relative; display: block; height: 348px;">
-      <img src="img/habitudes-home@2x.png" alt="RPLife capture" class="full-width" style="position: absolute; bottom: 0; right: 16px ;">
-    </div>
-    <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop">
-      <img src="img/habitudes-home@2x.png" alt="RPLife capture" class="full-width">
-    </div>
-  </div>
-  
-  <!-- Soon -->
-  <div class="grid-container block block-purple">
-    <div class="grid-33 hide-on-mobile margin-top-32" style="position: relative; display: block; height: 268px;">
-      <img src="img/soon-home@2x.png" alt="RPLife capture" class="full-width" style="position: absolute; bottom: 0; left: 16px ;">
-    </div>
-    <div class="grid-66">
-      <div class="app_title margin-top-64  margin-bottom-64 mobile-margin-bottom-32">
-        <h2 class="white">
-          <img src="img/novicon-question-mark@2x.png" alt="Unknown app icon" width="48" height="48">
-          Soon
-          <span>on iPhone</span>
-        </h2>
-        <h3 class="white">Overwhelmed by household chores?</h3>
-        <p class="white">There are good news for you coming soon…</p>
+
+    <!-- Habitudes -->
+    <div class="grid-100 block block-yellow">
+      <div class="grid-66">
+        <a class="undecored" href="habitudes.php">
+          <div class="app_title margin-top-64">
+            <h2 class="white">
+              <img src="img/habitudes-icon@2x.png" alt="RPLife icon" width="48" height="48" style="transform: translateY(-2px);">
+              Habitudes
+              <span>for iPhone</span>
+            </h2>
+          </div>
+        </a>
+        <h3 class="white">We are what we repeatedly do</h3>
+        <p class="white">
+          Use the power of habits and routines<br>
+          to improve yourself every day.
+        </p>
+        <div class="dl-buttons mobile-margin-bottom-32">
+          <a href="habitudes.php" class="button-app">Watch more</a><a href="https://apps.apple.com/us/app/habitudes/id1500560167" class="appstore-habitudes"><img src="img/appstore.svg" alt="Download Habitudes on the App Store"></a>
+        </div>
+      </div>
+      <div class="grid-33 hide-on-mobile margin-top-32" style="position: relative; display: block; height: 348px;">
+        <img src="img/habitudes-home@2x.png" alt="RPLife capture" class="full-width" style="position: absolute; bottom: 0; right: 16px ;">
+      </div>
+      <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop">
+        <img src="img/habitudes-home@2x.png" alt="RPLife capture" class="full-width">
       </div>
     </div>
-    <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop">
-      <img src="img/soon-home@2x.png" alt="RPLife capture" class="full-width">
+
+    <!-- Soon -->
+    <div class="grid-100 block block-purple">
+      <div class="grid-33 hide-on-mobile margin-top-32" style="position: relative; display: block; height: 268px;">
+        <img src="img/soon-home@2x.png" alt="RPLife capture" class="full-width" style="position: absolute; bottom: 0; left: 16px ;">
+      </div>
+      <div class="grid-66">
+        <div class="app_title margin-top-64  margin-bottom-64 mobile-margin-bottom-32">
+          <h2 class="white">
+            <img src="img/novicon-question-mark@2x.png" alt="Unknown app icon" width="48" height="48">
+            Soon
+            <span>on iPhone</span>
+          </h2>
+          <h3 class="white">Overwhelmed by household chores?</h3>
+          <p class="white">There are good news for you coming soon…</p>
+        </div>
+      </div>
+      <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop">
+        <img src="img/soon-home@2x.png" alt="RPLife capture" class="full-width">
+      </div>
     </div>
-  </div>
-  
-  <!-- Libs -->
-  <div class="grid-container block block-white">
-    <div class="grid-100 margin-top-64 margin-bottom-64">
-      <h2>Libraries</h2>
-      <h3>I create libs for your Swift applications.</h3>
-      <p class="margin-top-32">You can watch more on the <a href="libs.php">dedicated page</a>, <a href="https://github.com/sgigou">Github</a> or <a href="https://cocoapods.org/owners/45345">CocoaPods</a>.</p>
+
+    <!-- Libs -->
+    <div class="grid-50 block block-white margin-top-0">
+      <div class="grid-100 margin-top-64 margin-bottom-64">
+        <h2>Libraries</h2>
+        <h3>I create libs for your Swift applications.</h3>
+        <p class="margin-top-32"><a href="libs.php">Watch more</a></p>
+      </div>
     </div>
-  </div>
-  
-  <!-- Support -->
-  <div class="grid-container block block-white">
-    <div class="grid-100 margin-top-64">
-      <h2>Support</h2>
-      <h3 class="margin-bottom-32">Feel free to contact me.</h3>
-    </div>
-    <div class="grid-25 prefix-25 grid-parent margin-bottom-32">
-      <a href="https://twitter.com/stevegigou">
-        <div class="grid-100 margin-bottom-16"><img src="img/novicon-chat@2x.png" alt="" width="64" height="64"></div>
-        <div class="grid-100">On Twitter</div>
-      </a>
-    </div>
-    <div class="grid-25 margin-bottom-64">
-      <script type="text/javascript" language="javascript">
-      <!--
+
+    <!-- Support -->
+    <div class="grid-100 block block-white">
+      <div class="grid-100 margin-top-64">
+        <h2>Support</h2>
+        <h3 class="margin-bottom-32">Feel free to contact me.</h3>
+      </div>
+      <div class="grid-25 prefix-25 grid-parent margin-bottom-32">
+        <a href="https://twitter.com/stevegigou">
+          <div class="grid-100 margin-bottom-16"><img src="img/novicon-chat@2x.png" alt="" width="64" height="64"></div>
+          <div class="grid-100">On Twitter</div>
+        </a>
+      </div>
+      <div class="grid-25 margin-bottom-64">
+        <script type="text/javascript" language="javascript">
+          <!--
         // Email obfuscator script 2.1 by Tim Williams, University of Arizona
         // Random encryption key feature coded by Andrew Moulden
         // This code is freeware provided these four comment lines remain intact
         // A wizard to generate this code is at http://www.jottings.com/obfuscator/
         { coded = "AeHIH@MbMTF.Wo"
-          key = "U2iy9ZeEuwj8G4czbrYsHSFC637kmOvoRAfladhxJgNpITqVWQDKnXtMBPL051"
-          shift=coded.length
-          link=""
-          for (i=0; i<coded.length; i++) {
-            if (key.indexOf(coded.charAt(i))==-1) {
-              ltr = coded.charAt(i)
-              link += (ltr)
-            }
-            else {
-              ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
-              link += (key.charAt(ltr))
-            }
+        key = "U2iy9ZeEuwj8G4czbrYsHSFC637kmOvoRAfladhxJgNpITqVWQDKnXtMBPL051"
+        shift=coded.length
+        link=""
+        for (i=0; i<coded.length; i++) {
+          if (key.indexOf(coded.charAt(i))==-1) {
+            ltr = coded.charAt(i)
+            link += (ltr)
           }
-        document.write("<a href='mailto:"+link+"'>"+"<div class='grid-100 margin-bottom-16'><img src='img/novicon-email@2x.png' width='64' height='64'></div><div class='grid-100'>or by email.</div>"+"</a>")
+          else {
+            ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+            link += (key.charAt(ltr))
+          }
         }
+        document.write("<a href='mailto:"+link+"'>"+"<div class='grid-100 margin-bottom-16'><img src='img/novicon-email@2x.png' width='64' height='64'></div><div class='grid-100'>or by email.</div>"+"</a>")
+      }
         //-->
       </script>
       <noscript>Sorry, you need Javascript on to email me.</noscript>
     </div>
   </div>
-  
-  <div class="grid-container margin-top-64 margin-bottom-64">
+
+  <div class="grid-100 margin-top-64 margin-bottom-64">
     <h2>Who am I?</h2>
     <h3>I’m a french IT engineer</h3>
     <p>I make iOS applications and websites.<br>
       I believe in affordable apps and lifetime prices.<br>
       I’m also working on a <a href="https://en.wikipedia.org/wiki/Permaculture">permaculture farm</a>.</p>
+    </div>
+
   </div>
-    
+
   <?php include('footer-common.php') ?>
   <?php include('js.php') ?>
-    
+
 </body>
-    
+
 </html>
