f635f14853f0cf4dc1c728f92b86dee18fcd5009
Steve Gigou <steve@account.gigou.fr>
2020-06-03 17:24:57 +0200

    NF Code block
    And creation of the font dedicated page.

---
+++ fonts.php
@@ -0,0 +1,49 @@
+<!doctype.html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <?php include('head.php') ?>
+  <title>Fonts — Steve Gigou</title>
+</head>
+
+<body class="light">
+
+  <?php include('header.php') ?>
+
+  <div class="grid-container">
+
+    <div class="grid-container margin-top-64 margin-bottom-32">
+      <h1>I create fonts</h1>
+      <p class="subtitle">
+        They are <strong>free</strong><br>
+        and <strong>open-source</strong>.
+      </p>
+    </div>
+
+    <div class="grid-parent grid-100 block block-white font-code">
+      <div class="grid-100 margin-top-32 margin-bottom-32">
+        <h2 class="margin-bottom-16">NF Code</h2>
+        <p class="margin-bottom-16">A free proportional coding font with ligatures</p>
+        <div class="grid-100 body">
+          <h3>Features</h3>
+          <ul>
+            <li>Designed around clarity and readability.</li>
+            <li>Proportional font.</li>
+            <li>A lot of ligatures to enhance your code.</li>
+          </ul>
+          <h3>Examples</h3>
+          <img class="example" src="https://github.com/sgigou/NF-Code/raw/master/showcases/illustrations/liga_example.png" alt="Ligatures example">
+          <img class="example" src="https://github.com/sgigou/NF-Code/raw/master/showcases/examples/swift.png" alt="Swift example">
+          <img class="example" src="https://github.com/sgigou/NF-Code/raw/master/showcases/examples/cpp.png" alt="C++ example">
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <?php include('footer-common.php') ?>
+
+</body>
+
+</html>
