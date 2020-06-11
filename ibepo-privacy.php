2ce0cb4cf47b14ad9eff0ef4c827f4bb54fd9af9
Steve Gigou <steve@account.gigou.fr>
2020-06-11 13:54:09 +0200

    Create ibepo-privacy.php

---
+++ ibepo-privacy.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <?php include('head.php') ?>
+  <title>Vie privée — iBépo</title>
+</head>
+
+<body class="light">
+
+  <?php
+    include('header.php')
+  ?>
+
+  <div class="grid-container margin-top-32 margin-bottom-32">
+    <h1>Vie privée</h1>
+    <p class="subtitle margin-bottom-0">iBépo</p>
+  </div>
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <p>Steve Gigou a conçu l’application iBépo en tant qu’application Commerciale dont le code source est accessible. Ce service est fourni par Steve Gigou.</p>
+      <p>Cette page est utilisée pour informer les visiteurs de la politique d’utilisation et de recueil des données personnelles pour les personnes ayant décidé d’utiliser ce service.</p>
+      <p>Si vous utilisez mon service, vous acceptez la collecte et l’utilisation des informations telles que présentées sur cette page.</p>
+    </div>
+  </div>
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>Collecte et utilisation des informations</h2>
+      <p>Afin de garantir une sécurité optimale, iBépo n’utilise pas de logs en production. Les seuls logs qui pourraient être ajoutés aux fichiers du système le seront par iOS, et sont soumis à leurs conditions d’utilisation.</p>
+      <p>Je ne fais appel à AUCUN service tiers qui pourrait avoir accès à ces informations.</p>
+    </div>
+  </div>
+
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>Fournisseurs de service</h2>
+      <p>Je fais appel à des services tiers pour la gestion de ce projet.</p>
+      <p>Ces services n’ont accès à AUCUNE donnée personnelle.</p>
+    </div>
+  </div>
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>Sécurité</h2>
+      <p>iBépo est bâti autour de la sécurité et de la confidentialité.</p>
+      <p>L’absence de logs et de collecte de vos données vous garantit une sécurité maximale.</p>
+    </div>
+  </div>
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>Lien vers d’autres sites</h2>
+      <p>L’application contient des liens vers d’autres sites. Si vous cliquez sur ces liens, vous serez dirigé vers ces sites.</p>
+      <p>Veuillez noter que ces sites ne sont pas gérés par moi, et je vous encourage à aller lire les CGU de ces sites. Je n’ai aucun contrôle sur ces sites, et je n’assume pas la responsabilité des contenus ou pratiques de ces sites ou services.</p>
+    </div>
+  </div>
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>Modifications de cette page</h2>
+      <p>Je peux mettre à jour cette page de temps en temps. Vous êtes donc invités à la revoir après les mises à jour.</p>
+      <p>J’essaierai de vous prévenir des mises à jour à travers les historiques de version.</p>
+      <p>Ces modifications seront effectives dès leur publication sur cette page.</p>
+    </div>
+  </div>
+
+  <div class="grid-container text-block block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>Me contacter</h2>
+      <p>Si vous avez des doutes quant à l’utilisation de vos données, vous pouvez jeter un œil au <a href="https://github.com/sgigou/ibepo">code source de l’application</a>.</p>
+      <p>Si vous avez la moindre question, je vous invite à me contacter <a href="https://twitter.com/SteveGigou" title="Mon compte Twitter">sur Twitter</a>.</p>
+    </div>
+  </div>
+
+  <?php include('footer-common.php') ?>
+  <?php include('js.php') ?>
+
+</body>
+</html>
