00a5e95dc16da17fc4b74ab8881f742f09dad0e2
Steve Gigou <steve@account.gigou.fr>
2020-06-12 15:34:16 +0200

    iBepo blocks

--- ibepo.php
+++ ibepo.php
@@ -37,6 +37,81 @@
     </div>
   </div>
 
+  <div class="grid-container block block-white margin-top-0">
+    <div class="grid-66 margin-top-32 margin-bottom-32">
+      <h2>Bépo, mais pas trop</h2>
+      <h3>Une disposition pensée pour les appareils mobiles</h3>
+      <p>Le clavier iBépo reprend l’agencement Bépo, et l’adapte à une utilisation mobile.</p>
+    </div>
+    <div class="grid-33 margin-top-32 margin-bottom-32">
+      <img src="img/ibepo/access@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container block block-white">
+    <div class="grid-66 margin-top-32 margin-bottom-32">
+      <h2>Accédez à tous les caractères</h2>
+      <h3>D’une simple pression longue</h3>
+      <p>Si de nombreux caractères sont déjà visibles, bien d’autres se cachent sous ces touches.</p>
+    </div>
+    <div class="grid-33 margin-top-32 margin-bottom-32">
+      <img src="img/ibepo/popup@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container block block-white">
+    <div class="grid-66 margin-top-32 margin-bottom-32">
+      <h2>Autocorrection et suggestions</h2>
+      <h3>Profitez d’un dictionnaire performant</h3>
+      <p>iBépo s’appuie sur le dictionnaire d’iOS, et vous épaulera dans vos rédactions.</p>
+    </div>
+    <div class="grid-33 margin-top-32 margin-bottom-32">
+      <img src="img/ibepo/correct@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container block block-white">
+    <div class="grid-66 margin-top-32 margin-bottom-32">
+      <h2>Vitesse et précision</h2>
+      <h3>Un clavier optimisé et polyvalent</h3>
+      <p>L’optimisation est une priorité,  et le support de nombreux gestes le rend agréable à utiliser.</p>
+    </div>
+    <div class="grid-33 margin-top-32 margin-bottom-32">
+      <img src="img/ibepo/gestures@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container block block-white">
+    <div class="grid-66 margin-top-32 margin-bottom-32">
+      <h2>Intégré au système</h2>
+      <h3>Restez concentré sur vos tâches</h3>
+      <p>iBépo s’intègre parfaitement au système, par son design, son mode paysage et son mode sombre.</p>
+    </div>
+    <div class="grid-33 margin-top-32 margin-bottom-32">
+      <img src="img/ibepo/dark@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container block block-white">
+    <div class="grid-66 margin-top-32 margin-bottom-32">
+      <h2>La sécurité avant tout</h2>
+      <h3>Votre vie privée est précieuse</h3>
+      <p>Afin de vous garantir la sécurité de vos données, iBépo ne demande aucune autorisation et son code est accessible.</p>
+    </div>
+    <div class="grid-33 margin-top-32 margin-bottom-32">
+      <img src="img/ibepo/disposition@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container block block-white">
+    <div class="grid-100 margin-top-32 margin-bottom-32">
+      <h2>En savoir plus</h2>
+      <h3>Envie d’approfondir le sujet&nbsp;?</h3>
+      <p>Vous pouvez en apprendre plus sur la disposition Bépo sur <a href="https://bepo.fr/wiki/Accueil">bepo.fr</a>.</p>
+      <p>Vous pouvez également consulter la documentation et le code source d’iBépo sur <a href="https://github.com/sgigou/ibepo">la page du projet</a>.</p>
+    </div>
+  </div>
+
   <div class="grid-container margin-top-64">
     <h2 class="white">Téléchargez iBépo dès maintenant</h2>
     <div class="dl-buttons">
