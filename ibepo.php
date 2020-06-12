23a3d5e2368688bdbafe99deb94d8b662b43caf1
Steve Gigou <steve@account.gigou.fr>
2020-06-12 14:55:12 +0200

    iBepo dedicated page structure
    No block at the moment

---
+++ ibepo.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <?php include('head.php') ?>
+  <title>iBépo</title>
+</head>
+
+<body class="gray">
+
+  <?php include('header.php') ?>
+
+  <!-- Intro -->
+  <div class="grid-container margin-top-32" style="position: relative;">
+    <div class="grid-66 margin-top-32">
+      <div class="app_title app_title-page">
+        <h1 class="white">
+          <img src="img/ibepo-icon@2x.png" alt="Habitudes icon" width="56" height="56" style="transform: translateY(-3px);">
+          iBépo
+          <span>pour iPhone & iPad</span>
+        </h1>
+      </div>
+      <p class="subtitle white">
+        Le meilleur clavier Bépo<br>
+        pour vos appareils Apple.
+      </p>
+      <div class="dl-buttons">
+        <a href="https://apps.apple.com/us/app/habitudes/id1500560167" class="appstore-habitudes"><img src="img/appstore.svg" alt="Download Habitudes on the App Store"></a>
+      </div>
+    </div>
+    <div class="hide-on-mobile">
+      <img src="img/ibepo-page-home@2x.png" alt="Habitudes capture" style="height: 100%; position: absolute; top: -32px; right: 32px ;">
+    </div>
+    <div class="mobile-grid-70 mobile-prefix-15 hide-on-desktop margin-bottom-32">
+      <img src="img/ibepo/disposition@2x.png" class="shadowed full-width">
+    </div>
+  </div>
+
+  <div class="grid-container margin-top-64">
+    <h2 class="white">Téléchargez iBépo dès maintenant</h2>
+    <div class="dl-buttons">
+      <a href="https://apps.apple.com/us/app/habitudes/id1500560167" class="appstore-habitudes"><img src="img/appstore.svg" alt="Download Habitudes on the App Store"></a>
+    </div>
+  </div>
+
+  <?php include('footer-ibepo.php') ?>
+  <?php include('js.php') ?>
+
+</body>
+
+</html>
